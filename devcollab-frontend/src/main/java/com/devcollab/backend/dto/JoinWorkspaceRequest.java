0158eeace1f2d150196f827f43871762395972f5
PrashantBhanage <prashantbhanage717@gmail.com>
2026-05-28 12:55:23 +0530

    Ui is currently hardcoded

--- devcollab-frontend/src/main/java/com/devcollab/backend/dto/JoinWorkspaceRequest.java
+++
@@ -1,9 +0,0 @@
-package com.devcollab.backend.dto;
-
-import jakarta.validation.constraints.NotNull;
-
-public record JoinWorkspaceRequest(
-	@NotNull(message = "Workspace id is required")
-	Long workspaceId
-) {
-}
